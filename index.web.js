6e0bfdbf275ec1de6e93c34d33875f8825975a77
gaburak <misha.gaburak@gmail.com>
2017-02-08 00:59:14 +0200

    Added start component for all platform.

--- index.web.js
+++ index.web.js
@@ -1,56 +1,10 @@
-/**
- * React Native for Web Starter App
- * Follow me https://twitter.com/grabthecode
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
 } from 'react-native';
+import {App} from './src/App.js';
 
-export default class KoloSmaku extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React hello
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.web.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-
-AppRegistry.registerComponent('KoloSmaku', () => KoloSmaku);
+AppRegistry.registerComponent('KoloSmaku', () => App);
 AppRegistry.runApplication('KoloSmaku', {
   rootTag: document.getElementById('react-root')
 });
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+    Platform
+} from 'react-native';
+
+export class App extends Component {
+
+    render() {
+        return (
+            <Text>
+                Main App starting...
+            </Text>
+        )
+    }
+}
